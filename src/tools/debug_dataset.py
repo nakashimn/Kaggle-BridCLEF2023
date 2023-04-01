af281609a41063b01aa460d583875d4a552bbd4f
nakashimn <nakashimn@gmail.com>
2023-04-01 16:48:04 +0900

    modify:debug

--- src/tools/debug_dataset.py
+++ src/tools/debug_dataset.py
@@ -10,11 +10,12 @@ from sklearn.preprocessing import MultiLabelBinarizer
 import librosa
 import transformers
 import torch
+from torch.utils.data import DataLoader
 from torch.nn import functional as F
 sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))
 from config.sample import config
 from components.preprocessor import DataPreprocessor
-from components.datamodule import AudioClassificationDataset, BirdClefPredDataset
+from components.datamodule import BirdClefDataset, BirdClefPredDataset
 
 def fix_seed(seed):
     os.environ["PYTHONHASHSEED"] = str(seed)
@@ -31,9 +32,15 @@ def fix_seed(seed):
 data_preprocessor = DataPreprocessor(config)
 df_train = data_preprocessor.train_dataset_primary()
 df_test = data_preprocessor.test_dataset()
-df_pred = data_preprocessor.pred_dataset()
+df_pred = data_preprocessor.pred_dataset_for_submit()
 
 # FpDataSet
 fix_seed(config["random_seed"])
-dataset = AudioClassificationDataset(df_train, config["datamodule"]["dataset"])
-batch = dataset.__getitem__(0)
+dataset = BirdClefPredDataset(df_pred, config["datamodule"]["dataset"])
+batch = dataset.__getitem__(119)
+for i in tqdm(range(dataset.__len__())):
+    batch = dataset.__getitem__(i)
+
+dataloader = DataLoader(dataset, num_workers=0, batch_size=4, shuffle=False, drop_last=False)
+for data in tqdm(dataloader):
+    print(data)
