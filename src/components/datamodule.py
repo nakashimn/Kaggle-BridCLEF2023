8e78403b65c9fb2d61ad3c7e900a0252332fe050
nakashimn <nakashimn@gmail.com>
2023-05-27 00:32:44 +0900

    modify:for_oversampling

--- src/components/datamodule.py
+++ src/components/datamodule.py
@@ -224,6 +224,134 @@ class BirdClefMelspecDataset(Dataset):
         return [1 if l in series else 0 for l in self.config["labels"]]
 
 ################################################################################
+# For EfficientNetBaseModel(Context-rich Minority Oversampling)
+################################################################################
+class ContextRichMinorityOversampling:
+    def __init__(self, df_train, config):
+        self.config = config
+        self.df_train = df_train
+        self.labels, self.label_ratio = self._calc_label_ratio(df_train)
+
+    def _calc_label_ratio(self, df_train):
+        label_counts = df_train["labels"].value_counts()
+        labels = label_counts.index
+        inv_label_counts = [1/label_count for label_count in label_counts]
+        label_ratio = inv_label_counts / np.sum(inv_label_counts)
+        return labels, label_ratio
+
+    def get(self):
+        cutmix_ratio = np.random.beta(self.config["alpha"], self.config["alpha"])
+        img_fg, label_fg = self._pickup_fg()
+        img_bg, label_bg = self._pickup_bg()
+        img, label = self._cutmix(img_fg, img_bg, label_fg, label_bg, cutmix_ratio)
+        return img, label
+
+    def _cutmix(self, img_fg, img_bg, label_fg, label_bg, ratio):
+        cutwidth = int(img_fg.shape[1] * ratio)
+        offset = np.random.randint(0, img_fg.shape[1] - cutwidth)
+        img = img_bg
+        img[:, offset:offset + cutwidth] = img_fg[:, offset:offset + cutwidth]
+        label = label_fg * ratio + label_bg * (1 - ratio)
+        label = torch.tensor(label, dtype=torch.float32)
+        return img, label
+
+    def _pickup_fg(self):
+        labelnames_fg = np.random.choice(self.labels, p=self.label_ratio)
+        filepath_fg = self.df_train.loc[
+            self.df_train["labels"].str.get(0)==labelnames_fg[0], "filepath"
+        ].sample(1).values[0]
+        img_fg = np.load(filepath_fg)["arr_0"]
+        label_fg = self._to_onehot(labelnames_fg)
+        return img_fg, label_fg
+
+    def _pickup_bg(self):
+        sample_bg = self.df_train.sample(1)
+        filepath_bg = sample_bg["filepath"].values[0]
+        img_bg = np.load(filepath_bg)["arr_0"]
+        labelnames_bg = sample_bg["labels"].values[0]
+        label_bg = self._to_onehot(labelnames_bg)
+        return img_bg, label_bg
+
+    def _to_onehot(self, labels):
+        return np.array([1 if l in labels else 0 for l in self.config["labels"]])
+
+class BirdClefMelspecCMODataset(Dataset):
+    def __init__(self, df, config, transform=None):
+        self.config = config
+        self.oversampling_rate = config["cmo"]["oversampling_rate"]
+        self.cmo = ContextRichMinorityOversampling(df, self.config["cmo"])
+        self.filepaths = self._read_filepaths(df)
+        self.labels = None
+        if self.config["label"] in df.keys():
+            self.labels = self._read_labels(df[self.config["label"]])
+        self.pre_transform = A.Compose(
+            [A.Normalize(config["mean"], config["std"])]
+        )
+        self.to_tensor = Tv.ToTensor()
+        self.transform = transform
+
+    def __len__(self):
+        if self.transform is None:
+            return len(self.filepaths)
+        if self.oversampling_rate <= 1.0:
+            return len(self.filepaths)
+        return int(len(self.filepaths)*self.oversampling_rate)
+
+    def __getitem__(self, idx):
+        if idx < len(self.filepaths):
+            return self._pickup_org(idx)
+        return self._get_oversample()
+
+    def _pickup_org(self, idx):
+        melspec = self._read_melspec(self.filepaths[idx])
+        melspec = self._normalize(melspec)
+        melspec = self.pre_transform(image=melspec)["image"]
+        melspec = self.to_tensor(melspec)
+        if self.transform is not None:
+            melspec = self.transform(melspec)
+        if self.labels is not None:
+            labels = self.labels[idx]
+            return melspec, labels
+        return melspec
+
+    def _get_oversample(self):
+        melspec, labels = self.cmo.get()
+        melspec = np.expand_dims(melspec, axis=-1)
+        melspec = self._normalize(melspec)
+        melspec = self.pre_transform(image=melspec)["image"]
+        melspec = self.to_tensor(melspec)
+        if self.transform is not None:
+            melspec = self.transform(melspec)
+        if self.labels is not None:
+            return melspec, labels
+        return melspec
+
+    def _read_filepaths(self, df):
+        values = df["filepath"].values
+        return values
+
+    def _read_melspec(self, filepath):
+        melspec = np.load(filepath)["arr_0"]
+        melspec = np.expand_dims(melspec, axis=-1)
+        return melspec
+
+    def _normalize(self, melspec, eps=1e-6):
+        melspec = (melspec - melspec.mean()) / (melspec.std() + eps)
+        if (melspec.max() - melspec.min()) < eps:
+            return np.zeros_like(melspec, dtype=np.uint8)
+        melspec = (
+            255 * ((melspec - melspec.min()) / (melspec.max() - melspec.min()))
+        ).astype(np.uint8)
+        return melspec
+
+    def _read_labels(self, df):
+        labels = torch.tensor(df.apply(self._to_onehot), dtype=torch.float32)
+        return labels
+
+    def _to_onehot(self, series):
+        return [1 if l in series else 0 for l in self.config["labels"]]
+
+################################################################################
 # For EfficientNetBase Prediction
 ################################################################################
 class BirdClefMelspecPredDataset(Dataset):
@@ -396,6 +524,8 @@ class DataModule(LightningDataModule):
         return {"train": None, "valid": None, "pred": None}
 
     def train_dataloader(self):
+        if (self.df_train is None) or (len(self.df_train) == 0):
+            return None
         dataset = self.Dataset(
             self.df_train, self.config["dataset"], self.transforms["train"]
         )
@@ -409,6 +539,8 @@ class DataModule(LightningDataModule):
         return dataloader
 
     def val_dataloader(self):
+        if (self.df_val is None) or (len(self.df_val) == 0):
+            return None
         dataset = self.Dataset(
             self.df_val, self.config["dataset"], self.transforms["valid"]
         )
@@ -422,6 +554,8 @@ class DataModule(LightningDataModule):
         return dataloader
 
     def predict_dataloader(self):
+        if (self.df_pred is None) or (len(self.df_pred) == 0):
+            return None
         dataset = self.Dataset(
             self.df_pred, self.config["dataset"], self.transforms["pred"]
         )
