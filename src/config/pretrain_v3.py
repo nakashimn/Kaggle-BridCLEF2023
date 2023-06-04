a31cf0cdece6c571d2f70def8507dc9f3a583cef
nakashimn <nakashimn@gmail.com>
2023-06-04 11:58:54 +0900

    add:pretrain_config

---
+++ src/config/pretrain_v3.py
@@ -0,0 +1,224 @@
+config = {
+    "random_seed": 57,
+    "pred_device": "cpu",
+    "label": "labels",
+    "labels": [
+        "acafly", "acowoo", "afrsil1", "akekee", "akepa1", "akiapo",
+        "akikik", "aldfly", "ameavo", "amecro", "amegfi", "amekes",
+        "amepip", "amered", "amerob", "amewig", "amtspa", "andsol1",
+        "aniani", "annhum", "apapan", "arcter", "astfly", "azaspi1",
+        "babwar", "baleag", "balori", "banana", "banswa", "banwre1",
+        "barant1", "barpet", "barswa", "batpig1", "bawswa1", "bawwar",
+        "baywre1", "bbwduc", "bcnher", "belkin1", "belvir", "bewwre",
+        "bkbmag1", "bkbplo", "bkbwar", "bkcchi", "bkhgro", "bkmtou1",
+        "bknsti", "bkwpet", "blbgra1", "blbthr1", "blcjay1", "blctan1",
+        "blhpar1", "blkfra", "blknod", "blkpho", "blsspa1", "blugrb1",
+        "blujay", "bncfly", "bnhcow", "bobfly1", "bongul", "botgra",
+        "brant", "brbmot1", "brbsol1", "brcvir1", "brebla", "brnboo",
+        "brncre", "brnjay", "brnnod", "brnowl", "brnthr", "brratt1",
+        "brtcur", "brwhaw", "brwpar1", "btbwar", "btnwar", "btywar",
+        "bubsan", "bucmot2", "buffle", "buggna", "bugtan", "buhvir",
+        "bulori", "bulpet", "burpar", "burwar1", "bushti", "butsal1",
+        "buwtea", "cacgoo1", "cacwre", "calqua", "caltow", "cangoo",
+        "canvas", "canwar", "carchi", "carwre", "casfin", "caskin",
+        "caster1", "casvir", "categr", "ccbfin", "cedwax", "chbant1",
+        "chbchi", "chbsan", "chbwre1", "chcant2", "chemun", "chispa",
+        "chswar", "chukar", "cinfly2", "cintea", "clanut", "clcrob",
+        "cliswa", "cobtan1", "cocwoo1", "cogdov", "colcha1", "coltro1",
+        "comgal1", "comgol", "comgra", "comloo", "commer", "commyn",
+        "compau", "compea", "compot1", "comrav", "comsan", "comwax",
+        "comyel", "coohaw", "coopet", "cotfly1", "cowscj1", "cregua1",
+        "crehon", "creoro1", "crfpar", "cubthr", "daejun", "dowwoo",
+        "ducfly", "dunlin", "dusfly", "easblu", "easkin", "easmea",
+        "easpho", "eastow", "eawpew", "elepai", "eletro", "ercfra",
+        "eucdov", "eursta", "eurwig", "fepowl", "fiespa", "flrtan1",
+        "foxspa", "fragul", "gadwal", "gamqua", "gartro1", "gbbgul",
+        "gbwwre1", "gcrwar", "gilwoo", "glwgul", "gnttow", "gnwtea",
+        "gocfly1", "gockin", "gocspa", "goftyr1", "gohque1", "golphe",
+        "goowoo1", "grasal1", "grbani", "grbher3", "grcfly", "greegr",
+        "grefri", "grekis", "grepew", "gresca", "grethr1", "gretin1",
+        "greyel", "grhcha1", "grhowl", "grnher", "grnjay", "grtgra",
+        "grycat", "gryfra", "gryhaw2", "gwfgoo", "haiwoo", "hawama",
+        "hawcoo", "hawcre", "hawgoo", "hawhaw", "hawpet1", "heptan",
+        "hergul", "herthr", "herwar", "higmot1", "hofwoo1", "hoomer",
+        "houfin", "houspa", "houwre", "hudgod", "hutvir", "iiwi",
+        "incdov", "incter1", "indbun", "jabwar", "japqua", "kalphe",
+        "kauama", "kebtou1", "killde", "labwoo", "larspa", "laufal1",
+        "laugul", "layalb", "lazbun", "lcspet", "leafly", "leasan",
+        "leater1", "lesgol", "lesgre1", "lessca", "lesvio1", "lesyel",
+        "linspa", "linwoo1", "littin1", "lobdow", "lobgna5", "logshr",
+        "lotduc", "lotjae", "lotman1", "lucwar", "macwar", "madpet",
+        "magpet1", "magwar", "mallar3", "marwre", "masboo", "mastro1",
+        "mauala", "maupar", "meapar", "melbla1", "merlin", "mitpar",
+        "monoro1", "mouchi", "moudov", "mouela1", "mouqua", "mouwar",
+        "mutswa", "naswar", "norcar", "norfli", "norhar2", "normoc",
+        "norpar", "norpin", "norsho", "norwat", "nrwswa", "nutman",
+        "nutwoo", "oahama", "oaktit", "obnthr1", "ocbfly1", "oliwoo1",
+        "olsfly", "omao", "orbeup1", "orbspa1", "orcpar", "orcwar",
+        "orfpar", "osprey", "ovenbi1", "pabspi1", "pagplo", "palila",
+        "paltan1", "palwar", "parjae", "pasfly", "pavpig2", "pecsan",
+        "peflov", "perfal", "phivir", "pibgre", "pilwoo", "pinsis",
+        "pirfly1", "plawre1", "plaxen1", "plsvir", "plupig2", "pomjae",
+        "prowar", "puaioh", "purfin", "purgal2", "putfru1", "pygnut",
+        "rawwre1", "rcatan1", "rebnut", "rebsap", "rebwoo", "reccar",
+        "redava", "redcro", "redjun", "redpha1", "reevir1", "refboo",
+        "rehbar1", "relpar", "rempar", "reshaw", "rethaw", "rettro",
+        "rewbla", "ribgul", "rinduc", "rinkin1", "rinphe", "roahaw",
+        "robgro", "rocpig", "rorpar", "rotbec", "royter1", "rthhum",
+        "rtlhum", "ruboro1", "rubpep1", "rubrob", "rubwre1", "ruckin",
+        "rucspa1", "rucwar", "rucwar1", "rudpig", "rudtur", "ruff",
+        "rufhum", "rugdov", "rumfly1", "runwre1", "rutjac1", "saffin",
+        "sancra", "sander", "savspa", "saypho", "scamac1", "scatan",
+        "scbwre1", "scptyr1", "scrtan1", "semplo", "sheowl", "shicow",
+        "shtsan", "sibtan2", "sinwre1", "skylar", "sltred", "smbani",
+        "snogoo", "sobtyr1", "socfly1", "solsan", "sonspa", "sooshe",
+        "sooter1", "sopsku1", "sora", "soulap1", "spodov", "sposan",
+        "spotow", "spvear1", "squcuc1", "stbori", "stejay", "sthant1",
+        "sthwoo1", "strcuc1", "strfly1", "strsal1", "stvhum2", "subfly",
+        "sumtan", "swaspa", "swathr", "tenwar", "thbeup1", "thbkin",
+        "thswar1", "towsol", "treswa", "trogna1", "trokin", "tromoc",
+        "tropar", "tropew1", "tuftit", "tunswa", "veery", "verdin",
+        "vigswa", "wantat1", "warvir", "warwhe1", "wbwwre1", "webwoo1",
+        "wegspa1", "wesant1", "wesblu", "weskin", "wesmea", "wessan",
+        "westan", "wetshe", "wewpew", "whbman1", "whbnut", "whcpar",
+        "whcsee1", "whcspa", "whevir", "whfibi", "whfpar1", "whimbr",
+        "whiter", "whiwre1", "whtdov", "whtspa", "whttro", "whwbec1",
+        "whwdov", "wilfly", "willet1", "wilsni1", "wiltur", "wlswar",
+        "wooduc", "woothr", "wrenti", "y00475", "yebcar", "yebcha",
+        "yebela1", "yebfly", "yebori1", "yebsap", "yebsee1", "yefcan",
+        "yefgra1", "yegvir", "yehbla", "yehcar1", "yelgro", "yelwar",
+        "yeofly1", "yerwar", "yeteup1", "yetvir", "zebdov"
+    ],
+    "experiment_name": "birdclef2023-pretrain-v3",
+    "run_id": None,
+    "path": {
+        "traindata": "/workspace/kaggle/input/birdclef-2021-2022-pretrain/train_melspec/",
+        "trainmeta": "/workspace/kaggle/input/birdclef-2021-2022-pretrain/train_metadata.csv",
+        "testdata": None,
+        "preddata": None,
+        "temporal_dir": "../tmp/artifacts/",
+        "model_dir": "/kaggle/input/model/birdclef2023-pretrain-v3/",
+        "ckpt_dir": ""
+    },
+    "modelname": "best_loss",
+    "init_with_checkpoint": False,
+    "resume": False,
+    "upload_every_n_epochs": None,
+    "sampling_rate": 32000,
+    "chunk_sec": 5,
+    "duration_sec": 5,
+    "n_mels": 256,
+    "pred_ensemble": False,
+    "train_with_alldata": True
+}
+config["augmentation"] = {
+    "ClassName": "SpecAugmentation",
+    "time_shift": {
+        "probability": 0.5,
+        "max": 0.2
+    },
+    "freq_mask": {
+        "probability": 0.5,
+        "max": 130
+    },
+    "time_mask": {
+        "probability": 0.2,
+        "max": 30
+    },
+    "fadein": {
+        "probability": 0.5,
+        "max": 0.9
+    },
+    "fadeout": {
+        "probability": 0.3,
+        "max": 0.5
+    }
+}
+config["model"] = {
+    "ClassName": "BirdClefEfficientNetModel",
+    "base_model_name": "tf_efficientnet_b0_ns",
+    "input_channels": 1,
+    "num_class": 509,
+    "fc_mid_dim": 2560,
+    "n_mels": config["n_mels"],
+    "gradient_checkpointing": True,
+    "mixup": {
+        "alpha": 0.2
+    },
+    "label_smoothing": {
+        "eps": 0.01
+    },
+    "loss": {
+        "name": "nn.CrossEntropyLoss",
+        "params": {
+            "weight": None
+        }
+    },
+    "optimizer":{
+        "name": "optim.RAdam",
+        "params":{
+            "lr": 1e-3
+        },
+    },
+    "scheduler":{
+        "name": "optim.lr_scheduler.CosineAnnealingWarmRestarts",
+        "params":{
+            "T_0": 100,
+            "eta_min": 0,
+        }
+    },
+    "save_directory": "/workspace/data/model/birdclef2023_pretrained_v2/"
+}
+config["earlystopping"] = {
+    "patience": 5
+}
+config["checkpoint"] = {
+    "dirpath": config["path"]["model_dir"],
+    "save_top_k": 1,
+    "mode": "min",
+    "save_last": False,
+    "save_weights_only": False
+}
+config["trainer"] = {
+    "accelerator": "gpu",
+    "devices": 1,
+    "max_epochs": 100,
+    "accumulate_grad_batches": 8,
+    "deterministic": False,
+    "precision": 32
+}
+config["datamodule"] = {
+    "ClassName": "DataModule",
+    "dataset": {
+        "ClassName": "BirdClefMelspecDataset",
+        "base_model_name": config["model"]["base_model_name"],
+        "num_class": config["model"]["num_class"],
+        "label": config["label"],
+        "labels": config["labels"],
+        "mean": 0.485,
+        "std": 0.229,
+        "melspec": {
+            "sr": config["sampling_rate"],
+            "n_mels": config["n_mels"],
+            "n_fft": 2048,
+            "hop_length": 512,
+            "fmin": 16,
+            "fmax": 16386
+        },
+        "path": config["path"],
+        "chunk_sec": config["chunk_sec"],
+        "duration_sec": config["duration_sec"]
+    },
+    "dataloader": {
+        "batch_size": 32,
+        "num_workers": 8,
+    },
+}
+config["Metrics"] = {
+    "confmat": {
+        "label": config["labels"]
+    },
+    "cmAP": {
+        "padding_num": 5
+    }
+}
